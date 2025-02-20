e489dc47561d46cf2de034b0599201f964a8f928
vcgithub41210 <vinayc.11b.kmbvm@gmail.com>
2025-02-20 21:50:33 +0530

    landing page

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = ()=>{
+  return(
+  <div className="shadow-lg rounded-br-lg rounded-bl-lg shadow-[#5E7B51] h-[5rem] bg-[#86A677] w-[65rem] ">
+    </div>
+  )
+}
+export default Header;
