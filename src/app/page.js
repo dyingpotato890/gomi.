a9f5256a39c5c31a413a69f1dffd44b33b9e295c
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2025-02-21 22:44:22 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,9 @@ import Content from "@/components/Content"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-screen w-screen bg-[#E8E9F3]">
+    <div className="flex flex-col w-full h-screen bg-[url('@/landingpageimage.png')] bg-cover bg-center">
     <Header/>
-    <div className="flex w-[65rem] flex-1">
-      <Content/>
-      <Content/>
-    </div>
+    <h1 className="pl-10 text-white font-rethink font-bold text-[5rem]">Clicks <br/> Turned to <br/> Clean Streets </h1>
     </div>
   );
 }
