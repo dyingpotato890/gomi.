70f5d317324eb292e0213dacbcd3bed130db7de2
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2025-02-21 23:14:02 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = ()=>{
   return(
   <div className="h-[5rem] shadow-md w-full flex justify-center bg-[#214E34] ">
     <div className="h-[5rem] w-[50rem]">
-      <ul className= "flex w-full h-full justify-around items-center>
+      <ul className= "flex w-full h-full justify-around items-center">
         <li>Home</li>
         <li>Contact</li>
         <li>Login</li>
